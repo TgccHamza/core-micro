4bec8eed547dd3c77b44d9f7d6a5ff07d009b94f
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2024-11-08 17:46:06 +0100

    - added health endpoint

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from .database import engine, Base
 from .routers import project
 from .routers import arena
 from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import JSONResponse
+from typing import Dict, Any
 
 #Base.metadata.create_all(bind=engine)
 
@@ -18,6 +20,10 @@ async def custom_swagger_ui_html():
         swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css",
         swagger_favicon_url="https://fastapi.tiangolo.com/img/favicon.png"
     )
+
+@app.get("/health", response_model=Dict[str, Any])
+async def health_game():
+    return JSONResponse(content={'msg': 'Hello world'})
 
 app.include_router(project.router, tags=["projects"])
 app.include_router(arena.router, tags=["arenas"])
