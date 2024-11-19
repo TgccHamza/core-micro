7e0f4b7d8a35cafe47ba14c7a2711bb12b2a7070
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2024-11-19 17:47:02 +0100

    verify bug upload file

--- app/middlewares/CollabAuthMiddleware.py
+++ app/middlewares/CollabAuthMiddleware.py
@@ -7,7 +7,5 @@ class CollabAuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         print(f"Incoming request: {request.method} {request.url}")
         print("body request")
-        body = await request.body()
-        print("Request body:", body[1:200])  # Log request body
 
         return call_next, request
