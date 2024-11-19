bd4307e075537c5b18bd0ede5f9087b0182c778b
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2024-11-19 17:27:56 +0100

    verify bug upload file

--- app/middlewares/CollabAuthMiddleware.py
+++ app/middlewares/CollabAuthMiddleware.py
@@ -6,6 +6,8 @@ from starlette.middleware.base import BaseHTTPMiddleware
 class CollabAuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         print(f"Incoming request: {request.method} {request.url}")
-
+        print("body request")
+        body = await request.body()
+        print("Request body:", body)  # Log request body
 
         return call_next, request
